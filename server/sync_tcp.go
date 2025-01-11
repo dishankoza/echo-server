3a4f8a3b807163cfa6c14e59422675e6f7630642
Dishank <dishankoza@outlook.com>
2025-01-10 16:34:25 -0800

    added goroutines to support muiltple clients

--- server/sync_tcp.go
+++ server/sync_tcp.go
@@ -6,23 +6,38 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/dishankoza/echo-server/config"
 )
 
-func readCommand(c net.Conn) (string, error) {
-	//TODO making the read shot compatible with size > 512
+type ConnWrapper struct {
+	net.Conn
+}
 
-	var buf []byte = make([]byte, 512)
-	n, err := c.Read(buf[:])
-	if err != nil {
-		return "", err
+func (c *ConnWrapper) readCommand() (string, error) {
+	var buf []byte
+	tmp := make([]byte, 512)
+	for {
+		n, err := c.Read(tmp)
+		if err != nil {
+			log.Print(err == io.EOF)
+			if err == io.EOF {
+				return "", err
+			}
+			return "", err
+		}
+		buf = append(buf, tmp[:n]...)
+		if n < 512 {
+			break
+		}
 	}
-	return string(buf[:n]), nil
+	return string(buf), nil
 }
 
 func respond(cmd string, c net.Conn) error {
 	if _, err := c.Write([]byte(cmd)); err != nil {
+		log.Println("Error reading command:", err)
 		return err
 	}
 	return nil
@@ -31,7 +46,9 @@ func respond(cmd string, c net.Conn) error {
 func RunSyncServer() {
 	log.Println("Starting a synchronous TCP server on", config.Host, config.Port)
 
-	var con_client int = 0
+	var conClient int
+	var mu sync.Mutex // Mutex to protect conClient
+	var md sync.Mutex
 
 	lsnr, err := net.Listen("tcp", config.Host+":"+strconv.Itoa((config.Port)))
 
@@ -45,30 +62,43 @@ func RunSyncServer() {
 			panic(err)
 		}
 
-		con_client += 1
-		log.Println("Client connected with address", c.RemoteAddr(), "concurrent clients", con_client)
+		mu.Lock() // Acquire the mutex before modifying conClient
+		conClient++
+		log.Println("Client connected with address", c.RemoteAddr(), "concurrent clients", conClient)
+		mu.Unlock() // Release the mutex after modifying conClient
 
-		for {
-			cmd, err := readCommand(c)
-			if err != nil {
-				c.Close()
-				con_client -= 1
-				log.Println("Client Disconnected", c.RemoteAddr(), "concurrent clients", con_client)
-				if err == io.EOF {
-					break
-				}
-				log.Println("err", err)
-			}
-			cmds := strings.Split(cmd, "\n")
-			for _, cmd := range cmds {
-				if cmd != "" {
-					log.Println("Command", cmd)
-				}
-			}
+		go func(c net.Conn) {
+			defer c.Close()
+			defer func() {
+				md.Lock()
+				conClient--
+				log.Println("Client disconnected", c.RemoteAddr(), "concurrent clients", conClient)
+				md.Unlock()
+			}()
 
-			if err = respond(cmd, c); err != nil {
-				log.Println("err write:", err)
+			wrappedConn := &ConnWrapper{c}
+			for {
+				cmd, err := wrappedConn.readCommand()
+				if err != nil {
+					if err == io.EOF {
+						return
+					}
+					log.Println("Failed to read command:", err)
+					return
+				}
+
+				cmds := strings.Split(cmd, "\n")
+				for _, cmd := range cmds {
+					if cmd != "" {
+						log.Println("Command", cmd)
+					}
+				}
+
+				if err = respond(cmd, c); err != nil {
+					log.Println("Failed to respond:", err)
+					return
+				}
 			}
-		}
+		}(c)
 	}
 }
